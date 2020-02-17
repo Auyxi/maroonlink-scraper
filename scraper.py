acf577d1b57f002311455eff2b7c12070fa2c4cd
alex-d-n-pham <fragcakes@gmail.com>
2020-02-17 05:55:32 -0600

    finished scraping maroonlink

--- scraper.py
+++ scraper.py
@@ -7,16 +7,19 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
+
+import json
 
 load_dotenv(dotenv_path='info.env')
 
-# open browser
+# setup browser
 driver = webdriver.Chrome()
-driver.get("https://maroonlink.tamu.edu/organizations")
+driver.get('https://maroonlink.tamu.edu/organizations')
 
 # sign in process
-""" sign_in = driver.find_elements_by_xpath('//*[@id="discovery-bar"]/div/header[1]/div/a[2]')[0]
+sign_in = driver.find_elements_by_xpath('//*[@id="discovery-bar"]/div/header[1]/div/a[2]')[0]
 sign_in.click()
 
 # username
@@ -39,28 +42,54 @@ password_submit.click()
 
 # duo page
 WebDriverWait(driver, 10).until(EC.title_contains("Organizations"))
-# you have 10 seconds to manually click the button for duo and sign in """
+# you have 10 seconds to manually click the button for duo and sign in
 
 
-# tell orgs list to load everything
-""" load_more = driver.find_element_by_xpath('//*[@id="react-app"]/div/div[1]/div/div[2]/div/div[2]/div/div[2]/div[2]/button/div/div/span')
+# setup data for loading
+f = open('organizations.in', 'r')
+links = f.readlines()
+f.close()
+orgs = []
 
-while len(driver.find_elements_by_xpath('//*[@id="react-app"]/div/div[1]/div/div[2]/div/div[2]/div/div[2]/div[2]/button/div/div/span')) > 0:
-    load_more.click()
+# begin loading data
+for org in links:
+    driver.get(org)
+
+    x = {}
+
+    x["name"] = driver.find_element_by_css_selector("h1[style*='padding: 13px 0px 0px 85px;'").text
     try:
-        WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="react-app"]/div/div[1]/div/div[2]/div/div[2]/div/div[2]/div[2]/button/div/div/span')))
-    except TimeoutException:
-        break """
+        x["desc"] = driver.find_element_by_xpath("//*[@id='react-app']/div/div[1]/div/div/div[1]/div[1]/div/div[2]/p").text
+    except NoSuchElementException:
+        x["desc"] = ""
+    x["people"] = []
 
-load_more = driver.find_element_by_xpath('//*[@id="react-app"]/div/div[1]/div/div[2]/div/div[2]/div/div[2]/div[2]/button/div/div/span')
-load_more.click()
+    time.sleep(1)
 
-time.sleep(1)
+    menus = driver.find_elements_by_css_selector('span[style*="position: relative; opacity: 1; font-size: 16px; letter-spacing: 0px; text-transform: uppercase; font-weight: 500; margin: 0px; user-select: none; padding-left: 16px; padding-right: 16px; color: rgb(0, 0, 0);"')
+    if len(menus) == 1:
+        menus[0].click()
+    else:
+        menus[1].click()
+        
+    time.sleep(1)
 
-# print(len(driver.find_elements_by_css_selector('a[style*="display: block; text-decoration: none; margin-bottom: 20px;"')))
+    while len(driver.find_elements_by_css_selector('span[style*="position: relative; opacity: 1; font-size: 16px; letter-spacing: 0px; text-transform: uppercase; font-weight: 500; margin: 0px; user-select: none; padding-left: 16px; padding-right: 16px; color: rgb(0, 0, 0);"')) > 0:
+        driver.find_element_by_css_selector('span[style*="position: relative; opacity: 1; font-size: 16px; letter-spacing: 0px; text-transform: uppercase; font-weight: 500; margin: 0px; user-select: none; padding-left: 16px; padding-right: 16px; color: rgb(0, 0, 0);"').click()
+        try:
+            WebDriverWait(driver, 3).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'span[style*="position: relative; opacity: 1; font-size: 16px; letter-spacing: 0px; text-transform: uppercase; font-weight: 500; margin: 0px; user-select: none; padding-left: 16px; padding-right: 16px; color: rgb(0, 0, 0);"')))
+        except TimeoutException:
+            break   
 
-orgs_list = driver.find_elements_by_css_selector('a[style*="display: block; text-decoration: none; margin-bottom: 20px;"')
+    people_list = driver.find_elements_by_css_selector('div[style="font-size: 16px; overflow: hidden; text-overflow: ellipsis;"')
 
-for org in orgs_list:
-    print(org.get_attribute('href'))
+    for person in people_list:
+        x["people"].append(person.text)
 
+    orgs.append(x)
+    print(x)
+
+# write out json data
+f = open('org_data.json', 'w+')
+f.write(json.dumps(orgs))
+f.close()
